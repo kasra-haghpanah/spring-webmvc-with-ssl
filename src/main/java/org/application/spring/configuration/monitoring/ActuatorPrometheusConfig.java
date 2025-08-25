1f832af700fa4ddd6267e16ad9f906f88b93beca
kasra-haghpanah <kasrakhpk1985@gmail.com>
2025-08-26 01:05:26 +0330

    add grafana + prometheus + actuator for monitoring & alerting

---
+++ src/main/java/org/application/spring/configuration/monitoring/ActuatorPrometheusConfig.java
@@ -0,0 +1,33 @@
+package org.application.spring.configuration.monitoring;
+
+
+
+
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.micrometer.metrics.autoconfigure.export.prometheus.PrometheusProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ActuatorPrometheusConfig {
+
+    @Bean
+    @Primary
+    public WebEndpointProperties webEndpointProperties() {
+        WebEndpointProperties properties = new WebEndpointProperties();
+        properties.getExposure().getInclude().add("health");
+        properties.getExposure().getInclude().add("info");
+        properties.getExposure().getInclude().add("prometheus");
+        return properties;
+    }
+
+    @Bean
+    public PrometheusProperties prometheusProperties() {
+        PrometheusProperties properties = new PrometheusProperties();
+        properties.setEnabled(true);
+        return properties;
+    }
+}
+
+
