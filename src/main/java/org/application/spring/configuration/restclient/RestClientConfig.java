1efec055d8f20c40f8cb072070da3a2eaf77480d
kasra-haghpanah <kasrakhpk1985@gmail.com>
2025-09-03 01:46:31 +0330

    add profile to pom.xml

--- src/main/java/org/application/spring/configuration/restclient/RestClientConfig.java
+++ src/main/java/org/application/spring/configuration/restclient/RestClientConfig.java
@@ -1,6 +1,5 @@
 package org.application.spring.configuration.restclient;
 
-import org.application.spring.configuration.properties.Properties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
