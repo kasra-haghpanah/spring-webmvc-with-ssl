af765b0df45160c09e4d6df430f5a9301d1026d3
kasra-haghpanah <kasrakhpk1985@gmail.com>
2025-10-19 11:13:32 +0330

    add spring-apectj

--- src/main/java/org/application/spring/configuration/jackson/JacksonConfig.java
+++ src/main/java/org/application/spring/configuration/jackson/JacksonConfig.java
@@ -3,7 +3,6 @@ package org.application.spring.configuration.jackson;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -11,22 +10,14 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.application.spring.configuration.properties.Properties;
 import org.application.spring.configuration.security.SecurityConfig;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
 
 import java.io.IOException;
-import java.time.format.DateTimeFormatter;
 import java.util.TimeZone;
 
 @DependsOn({"properties"})
@@ -37,32 +28,26 @@ public class JacksonConfig {
     private static final String dateTimeFormat = "yyyy-MM-dd HH:mm:ss";
     private static final String localTimeFormat = "HH:mm:ss";
 
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
 
-    @Bean("customStringModule")
-    public SimpleModule customStringModule() {
         SimpleModule module = new SimpleModule();
         module.addDeserializer(String.class, new JsonDeserializer<String>() {
             @Override
-            public String deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
-                String original = jsonParser.getText();
+            public String deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+                String original = p.getText();
                 return SecurityConfig.sanitize(original);
             }
         });
-        return module;
-    }
 
-
-    @Bean
-    @Primary
-    public ObjectMapper objectMapper(@Qualifier("customStringModule") SimpleModule customStringModule) {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(customStringModule);
+        mapper.registerModule(module);
         mapper.setTimeZone(TimeZone.getTimeZone(Properties.getJacksonTimeZone()));
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
         mapper.registerModule(new JavaTimeModule());
         return mapper;
     }
-
 }
 
